dec941f91fa8b994a79c802a8e67384f97991c92
himanshugehlot280 <87193425+himanshugehlot280@users.noreply.github.com>
2024-04-29 15:27:53 +0530

    Added Docs

---
+++ utils.py
@@ -0,0 +1,24 @@
+from langchain.schema import Document
+from PyPDF2 import PdfReader
+
+# PDF UPLOAD
+def get_pdf_text(pdf_doc):
+        text = ""
+        pdf_reader = PdfReader(pdf_doc)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        return text
+
+def create_docs(pdf_file , filename):
+    # for pdf_file in pdf_files:
+    docs = []
+    if filename.lower().endswith(".pdf"):
+        pdf_data = get_pdf_text(pdf_file)
+        docs.append(Document(
+            page_content=pdf_data,
+            metadata={"filename": filename}
+         ))
+    else:
+        return("invalid pdf")
+
+    return docs
