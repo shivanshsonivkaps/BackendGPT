8c47820d4394b537b0ab9b1c4aa2bb16e1880fc8
Nidhi-Ranawat <ranawatnidhi23@gmail.com>
2024-04-29 13:38:08 +0530

    code-refactored

--- app.py
+++ app.py
@@ -1,120 +1,11 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS, cross_origin
-from langchain_core.messages import AIMessage, HumanMessage
-from langchain_openai import ChatOpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-from langchain.schema import Document
-from PyPDF2 import PdfReader
-import os
-import time
-import json
-from dotenv import load_dotenv
-
-from langchain_community.vectorstores import Pinecone
-from langchain_community.vectorstores import Pinecone as PineconeStore
-from langchain.vectorstores import Pinecone
-from pinecone import Pinecone
-
-load_dotenv()
-PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
-embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+from utils import *
+from pine import *
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-
-
-# CHAT APP FUNCTIONS
-def get_vectorstore():
-    vector_store = PineconeStore.from_existing_index(index_name="reserch",embedding=embeddings)
-    return vector_store
-
-def pull_from_pinecone(pinecone_apikey,pinecone_environment,pinecone_index_name,embeddings):
-    time.sleep(10)
-    pinecone = Pinecone(
-        api_key=pinecone_apikey,environment=pinecone_environment
-    )
-    index_name = pinecone_index_name
-    index = PineconeStore.from_existing_index(index_name, embeddings)
-    return index
-
-def similar_docs(query,pinecone_apikey,pinecone_environment,pinecone_index_name,embeddings):
-    pinecone = Pinecone(
-        api_key=pinecone_apikey,environment=pinecone_environment
-    )
-    index_name = pinecone_index_name
-    index = pull_from_pinecone(pinecone_apikey,pinecone_environment,index_name,embeddings)
-
-    index_stat = pinecone.Index(pinecone_index_name)
-    vector_count = index_stat.describe_index_stats()
-    k = vector_count["total_vector_count"]
-
-    similar_docs = index.similarity_search(query, 2)
-    sources = []
-    for similar_doc in similar_docs:
-        metadata = similar_doc.metadata
-        sources.append(metadata.get("file"))
-    return sources
-
-def get_context_retriever_chain(vector_store):
-    llm = ChatOpenAI()
-    retriever = vector_store.as_retriever()
-    prompt = ChatPromptTemplate.from_messages([
-      MessagesPlaceholder(variable_name="chat_history"),
-      ("user", "{input}"),
-      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
-    ])
-    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
-    return retriever_chain
-
-def get_conversational_rag_chain(retriever_chain):
-    llm = ChatOpenAI()
-    prompt = ChatPromptTemplate.from_messages([
-      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
-      MessagesPlaceholder(variable_name="chat_history"),
-      ("user", "{input}"),
-    ])
-    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
-    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
-
-
-# PDF UPLOAD
-def get_pdf_text(pdf_doc):
-        text = ""
-        pdf_reader = PdfReader(pdf_doc)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-        return text
-
-def push_to_pinecone(pinecone_apikey,pinecone_environment,pinecone_index_name,embeddings,docs):
-    text_splitter = RecursiveCharacterTextSplitter()
-    document_chunks = text_splitter.split_documents(docs)
-    pinecone = Pinecone(
-        api_key=pinecone_apikey,environment=pinecone_environment
-        )
-    # create a vectorstore from the chunks
-    vector_store=PineconeStore.from_documents(document_chunks,embeddings,index_name=pinecone_index_name)
-
-
-def create_docs(pdf_file , filename):
-    # for pdf_file in pdf_files:
-    docs = []
-    if filename.lower().endswith(".pdf"):
-        pdf_data = get_pdf_text(pdf_file)
-        docs.append(Document(
-            page_content=pdf_data,
-            metadata={"filename": filename}
-         ))
-    else:
-        return("invalid pdf")
-
-    return docs
-
 
 @app.route("/")
 def home():
@@ -140,7 +31,7 @@ def qa():
                 "chat_history": chat_history,
                 "input": user_input
             })
-            source = similar_docs(user_input,PINECONE_API_KEY,"us-east-1","reserch",embeddings)
+            source = similar_docs(user_input)
             response = response['answer'] + f"\n\nSource : {source}"
 
             data = {"question": user_input, "answer": response}
@@ -157,27 +48,19 @@ def qa():
 @app.route('/receive_pdf', methods=["GET",'POST'])
 def receive_pdf():
     try:
-        #Getting files from API
-         files = request.files
-         file = files.get('Demo')
-         file_name = file.filename
-         newPdf = create_docs(file , file_name)
+    #Getting files from API
+        files = request.files
+        file = files.get('Demo')
+        file_name = file.filename
+        newPdf = create_docs(file , file_name)
 
-        #Extract embeddings and docs from the PDF file
-         embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        #Push embeddings and documents to Pinecone
-         apikey = "a5d206d0-b27f-4a37-8947-8beaba28ee4b"
-         pinecone_environment = 'us-east-1'
-         pinecone_index_name = "reserch"
-         push_to_pinecone(apikey, pinecone_environment, pinecone_index_name, embeddings, newPdf)
-         return jsonify("OK"),200
+        push_to_pinecone(newPdf)
+        return jsonify("OK"),200
     except Exception as e:
         print(e)
         return({
             "status" : "500",
             "message" : "error"
         }),400
-
-
 
 app.run(debug=True, port=5001)
---
+++ pine.py
@@ -0,0 +1,80 @@
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.vectorstores import Pinecone
+from langchain_community.vectorstores import Pinecone as PineconeStore
+from langchain.vectorstores import Pinecone
+from pinecone import Pinecone
+import os
+import time
+
+from dotenv import load_dotenv
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+def get_vectorstore():
+    vector_store = PineconeStore.from_existing_index(index_name="newcsv",embedding=embeddings)
+    return vector_store
+
+def push_to_pinecone(docs,pinecone_environment="us-east-1",pinecone_index_name="newcsv"):
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(docs)
+    pinecone = Pinecone(
+        api_key=PINECONE_API_KEY,environment=pinecone_environment
+        )
+    # create a vectorstore from the chunks
+    vector_store=PineconeStore.from_documents(document_chunks,embeddings,index_name=pinecone_index_name)
+
+def pull_from_pinecone(pinecone_environment,pinecone_index_name,embeddings):
+    time.sleep(10)
+    pinecone = Pinecone(
+        api_key=PINECONE_API_KEY,environment=pinecone_environment
+    )
+    index_name = pinecone_index_name
+    index = PineconeStore.from_existing_index(index_name, embeddings)
+    return index
+
+def similar_docs(query,pinecone_environment="us-east-1",pinecone_index_name="newcsv"):
+    pinecone = Pinecone(
+        api_key=PINECONE_API_KEY,environment=pinecone_environment
+    )
+    index_name = pinecone_index_name
+    index = pull_from_pinecone(PINECONE_API_KEY,pinecone_environment,index_name,embeddings)
+
+    index_stat = pinecone.Index(pinecone_index_name)
+    vector_count = index_stat.describe_index_stats()
+    k = vector_count["total_vector_count"]
+
+    similar_docs = index.similarity_search(query, 2)
+    sources = []
+    for similar_doc in similar_docs:
+        metadata = similar_doc.metadata
+        sources.append(metadata.get("file"))
+    return sources
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI()
+    retriever = vector_store.as_retriever()
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    return retriever_chain
+
+def get_conversational_rag_chain(retriever_chain):
+    llm = ChatOpenAI()
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
